54646874fb504fc6666a2c98436494923b99a73a
Nikhil Wani <nikhil.s.wani@gmail.com>
2020-12-04 09:31:28 +0530

    added delete option and ordered by date

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
 
 urlpatterns = [ 
     # path('',views.home, name="home"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('article/<int:pk>',ArticleDetailView.as_view(), name = 'article-detail'),
     path('add_post/',AddPostView.as_view(), name = 'add_post'),
     path('article/update/<int:pk>', UpdatePostView.as_view(), name= "update_post"),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name= "delete_post"),
 ]
